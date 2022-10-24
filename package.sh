9bcafa5bad546d5a8667a127f925aa367f7cee12
Matthew Bell <mbell.dvsa@icloud.com>
2022-10-24 09:56:40 +0100

    Fix husky builds

--- package.sh
+++ package.sh
@@ -4,7 +4,8 @@ dist_dir="dist"
 mkdir -p ${dist_dir}
 cp -r build/src/* ${dist_dir}
 cp LICENSE README.md package.json ${dist_dir}
-pushd ${dist_dir} 
+jq 'del(.scripts.prepare)' dist/package.json > dist/_temp-package.json && mv dist/_temp-package.json dist/package.json
+pushd ${dist_dir}
 find . -type f -name "*.spec.*" -exec rm -f {} \;
 find . -name "__*__" -type d -empty -delete
 popd
