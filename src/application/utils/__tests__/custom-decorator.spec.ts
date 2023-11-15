6a577f0b6f481b7f9dcbb672267c05870dbd23fb
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2023-11-15 10:51:10 +0000

    Custom decorators pattern (#57)
    * Add custom decorators
    
    * Update test expectation
    
    * Add TS generic input for class definition
    
    * Rename outer paramName to param
    
    * Add 'NotNullQueryParam'
    
    * Logger decorator

---
+++ src/application/utils/__tests__/custom-decorator.spec.ts
@@ -0,0 +1,129 @@
+import {
+    APIGatewayEventRequestContextWithAuthorizer,
+    APIGatewayProxyEvent,
+    APIGatewayProxyEventPathParameters,
+} from 'aws-lambda';
+import {Logger, NonNullPathParam, NonNullQueryParam, ValidatePathParam, ValidateRole} from "../custom-decorators";
+import * as resp from "../../api/create-response";
+import {ExaminerRole} from "../../../domain/examiner-role";
+import {HttpStatus} from "../../api/http-status";
+
+describe('CustomDecorators', () => {
+    beforeEach(() => {
+        spyOn(resp, 'createResponse');
+    });
+
+    describe('NonNullPathParam', () => {
+        class TestClass {
+            @NonNullPathParam('testParam')
+            async handler(event: APIGatewayProxyEvent) {
+                return {statusCode: 200};
+            }
+        }
+
+        it('should return 400 error with object/param missing message', async () => {
+            await new TestClass().handler({pathParameters: null} as APIGatewayProxyEvent);
+            expect(resp.createResponse).toHaveBeenCalledWith(
+                'Event object or path parameters are missing.', HttpStatus.BAD_REQUEST,
+            );
+        });
+
+        it('should continue with handler code if passes decorator checks', async () => {
+            const data = await new TestClass().handler({
+                pathParameters: {testParam: 'test'} as APIGatewayProxyEventPathParameters,
+            } as APIGatewayProxyEvent);
+
+            expect(data.statusCode).toEqual(200);
+        });
+    });
+
+    describe('ValidateRole', () => {
+        class TestClass {
+            @ValidateRole(ExaminerRole.DE)
+            async handler(event: APIGatewayProxyEvent) {
+                return {statusCode: 200};
+            }
+        }
+
+        it('should return 401 with examiner role error', async () => {
+            await new TestClass().handler({} as APIGatewayProxyEvent);
+            expect(resp.createResponse).toHaveBeenCalledWith(
+                'Examiner role is not authorised for this request.', HttpStatus.UNAUTHORIZED,
+            );
+        });
+
+        it('should continue with handler code if passes decorator checks', async () => {
+            const data = await new TestClass().handler({
+                requestContext: {
+                    authorizer: {examinerRole: ExaminerRole.DE},
+                } as APIGatewayEventRequestContextWithAuthorizer<any>,
+            } as APIGatewayProxyEvent);
+
+            expect(data.statusCode).toEqual(200);
+        });
+    });
+
+    describe('ValidatePathParam', () => {
+        class TestClass {
+            @ValidatePathParam<TestClass>('test', (param: string) => param === '1234')
+            async handler(event: APIGatewayProxyEvent) {
+                return {statusCode: 200};
+            }
+        }
+
+        it('should return 400 with invalid error', async () => {
+            await new TestClass().handler({
+                pathParameters: {'something': '1234'} as APIGatewayProxyEventPathParameters,
+            } as APIGatewayProxyEvent);
+            expect(resp.createResponse).toHaveBeenCalledWith(
+                'Path parameter failed validation: "test"', HttpStatus.BAD_REQUEST,
+            );
+        });
+
+        it('should continue with handler code if passes decorator checks', async () => {
+            const data = await new TestClass().handler({
+                pathParameters: {'test': '1234'} as APIGatewayProxyEventPathParameters,
+            } as APIGatewayProxyEvent);
+
+            expect(data.statusCode).toEqual(200);
+        });
+    });
+
+    describe('NonNullQueryParam', () => {
+        class TestClass {
+            @NonNullQueryParam('testParam')
+            async handler(event: APIGatewayProxyEvent) {
+                return {statusCode: 200};
+            }
+        }
+
+        it('should return 400 error with object/query param missing message', async () => {
+            await new TestClass().handler({pathParameters: null} as APIGatewayProxyEvent);
+            expect(resp.createResponse).toHaveBeenCalledWith(
+                'Event object or query string parameters are missing.', HttpStatus.BAD_REQUEST,
+            );
+        });
+
+        it('should continue with handler code if passes decorator checks', async () => {
+            const data = await new TestClass().handler({
+                queryStringParameters: {testParam: 'test'} as APIGatewayProxyEventPathParameters,
+            } as APIGatewayProxyEvent);
+
+            expect(data.statusCode).toEqual(200);
+        });
+    });
+
+    describe('Logger', () => {
+        class TestClass {
+            @Logger('function-name')
+            async handler(event: APIGatewayProxyEvent) {
+                return {statusCode: 200};
+            }
+        }
+
+        it('should always allow through filter', async () => {
+            const data = await new TestClass().handler({} as APIGatewayProxyEvent);
+            expect(data.statusCode).toEqual(200);
+        });
+    });
+});
---
+++ src/application/utils/custom-decorators.ts
@@ -0,0 +1,145 @@
+import {bootstrapLogging, error} from "./logger";
+import {createResponse} from "../api/create-response";
+import {HttpStatus} from "../api/http-status";
+import {ExaminerRole} from "../../domain/examiner-role";
+import {getRoleFromRequestContext} from "../../framework/security/authorisation";
+
+/**
+ * Decorator method used to validate path parameters being defined
+ * If there are no pathParameters - Return 400
+ * If the specified pathParameter is not defined - Return 400
+ * @param {string} param
+ * @constructor
+ */
+export function NonNullPathParam<T>(param: string) {
+    return function (_target: T, _propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+
+        descriptor.value = async function (...args: any[]) {
+            const event = args[0];
+
+            // Check if the event and pathParameters are defined
+            if (!event || !event.pathParameters) {
+                error('Event object or path parameters are missing.');
+                return createResponse('Event object or path parameters are missing.', HttpStatus.BAD_REQUEST);
+            }
+
+            // Check for the specific path parameter
+            if (!event.pathParameters[param]) {
+                error('Path parameter not defined', param);
+                return createResponse(`Path parameter is required: ${param}`, HttpStatus.BAD_REQUEST);
+            }
+
+            // Call the original method if the check passes
+            return originalMethod.apply(this, args);
+        };
+    };
+}
+
+/**
+ * Decorator method used to validate whether the defined role is equal to the request context
+ * If there is no role in request context or the role is not equal to passed examinerRole - Return 401
+ * @param {ExaminerRole} examinerRole
+ * @constructor
+ */
+export function ValidateRole<T>(examinerRole: ExaminerRole) {
+    return function (_target: T, _propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+
+        descriptor.value = async function (...args: any[]) {
+            const event = args[0];
+
+            const role = getRoleFromRequestContext(event.requestContext);
+            if (!role || (role !== examinerRole)) {
+                error('Examiner role is not authorised for this request.', {required: examinerRole, actual: role});
+                return createResponse('Examiner role is not authorised for this request.', HttpStatus.UNAUTHORIZED);
+            }
+
+            // Call the original method if the check passes
+            return originalMethod.apply(this, args);
+        };
+    };
+}
+
+/**
+ * Decorator method used to validate path parameters using a validator function
+ * If there are no pathParameters - Return 400
+ * If param does not return truth from the validator function - Return 400
+ * @param {string} param
+ * @param {Function} validator
+ * @constructor
+ */
+export function ValidatePathParam<T>(param: string, validator: Function) {
+    return function (_target: T, _propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+
+        descriptor.value = async function (...args: any[]) {
+            const event = args[0];
+
+            if (!event || !event.pathParameters) {
+                error('Event object or path parameters are missing.');
+                return createResponse('Event object or path parameters are missing.', HttpStatus.BAD_REQUEST);
+            }
+
+            const pathParam = event.pathParameters[param];
+
+            if (!validator(pathParam)) {
+                error('Path parameter is invalid.', {param, value: pathParam});
+                return createResponse(`Path parameter failed validation: \"${param}\"`, HttpStatus.BAD_REQUEST);
+            }
+
+            // Call the original method if the check passes
+            return originalMethod.apply(this, args);
+        };
+    };
+}
+
+/**
+ * Decorator method used to validate query string parameters being defined
+ * If there are no queryStringParameters - Return 400
+ * If the specified queryStringParameters is not defined - Return 400
+ * @param {string} param
+ * @constructor
+ */
+export function NonNullQueryParam<T>(param: string) {
+    return function (_target: T, _propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+
+        descriptor.value = async function (...args: any[]) {
+            const event = args[0];
+
+            if (!event || !event.queryStringParameters) {
+                error('Event object or query string parameters are missing.');
+                return createResponse('Event object or query string parameters are missing.', HttpStatus.BAD_REQUEST);
+            }
+
+            // Check for the specific query parameter
+            if (!event.queryStringParameters[param]) {
+                error('Query parameter not defined', param);
+                return createResponse(`Query parameter is required: ${param}`, HttpStatus.BAD_REQUEST);
+            }
+
+            // Call the original method if the check passes
+            return originalMethod.apply(this, args);
+        };
+    };
+}
+
+/**
+ * Decorator to use when want to initialise logging for a function
+ * @param {string} functionName
+ * @constructor
+ */
+export function Logger<T>(functionName: string) {
+    return function (_target: T, _propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+
+        descriptor.value = async function (...args: any[]) {
+            const event = args[0];
+
+            bootstrapLogging(functionName, event);
+
+            return originalMethod.apply(this, args);
+        };
+    };
+}
