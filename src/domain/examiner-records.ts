566a5148310ac33e87e9eb84153aaed35ace2f09
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-01-21 14:10:43 +0000

    Change examiner records appRef to string

--- src/domain/examiner-records.ts
+++ src/domain/examiner-records.ts
@@ -6,7 +6,7 @@ import { Circuit } from "@dvsa/mes-test-schema/categories/AM1";
  * Interface for Examiner Records
  */
 export interface ExaminerRecordModel {
-  appRef: number,
+  appRef: string,
   testCategory: TestCategory,
   activityCode: number,
   testCentre: TestCentre,
