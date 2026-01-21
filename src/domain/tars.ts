531e9b79bed4d5410e8c9b7fee9fe63db8cfb6b2
RLCorp <rhys.lewis@dvsa.gov.uk>
2026-01-21 13:53:19 +0000

    Changed app ref formatter to account for DSP

--- src/domain/tars.ts
+++ src/domain/tars.ts
@@ -1,16 +1,28 @@
 import { ApplicationReference } from '@dvsa/mes-test-schema/categories/common';
 
 /**
+ * Gets either a formatted app ref or the booking reference, depending on whether the booking reference is available.
+ *
+ * @param appRef The application reference, as separate fields
+ * @returns The booking reference or the formatted application reference
+ */
+
+export const getFormattedApplicationReference = (appRef: ApplicationReference): string => {
+  if (appRef.bookingReference) {
+    return appRef.bookingReference
+  }
+  return formatApplicationReference(appRef).toString();
+};
+
+
+/**
  * Formats application reference as a single number, of the form <``app-id``><``book-seq``><``check-digit``>.
  *
  * @param appRef The application reference, as separate fields
  * @returns The app id, booking sequence (padded to 2 digits) and check digit
  */
-export const formatApplicationReference = (appRef: ApplicationReference): number => {
+
+export const formatApplicationReference = (appRef: ApplicationReference): Number => {
   const formatter = Intl.NumberFormat('en-gb', { minimumIntegerDigits: 2 });
-  return Number(
-      appRef.applicationId.toString()
-      + formatter.format(appRef.bookingSequence)
-      + appRef.checkDigit.toString()
-  );
+  return Number(`${appRef.applicationId}${formatter.format(Number(appRef.bookingSequence))}${appRef.checkDigit}`);
 };
